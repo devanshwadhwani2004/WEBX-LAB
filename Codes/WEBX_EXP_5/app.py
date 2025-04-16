3099485766640b34508dfd0aed7ca378de59111a
devanshwadhwani2004 <140874678+devanshwadhwani2004@users.noreply.github.com>
2025-04-16 11:20:40 -0700

    Add files via upload

---
+++ Codes/WEBX_EXP_5/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/user/<username>')
+def user(username):
+    return render_template('user.html', username=username)
+
+if __name__ == '__main__':
+    app.run(debug=True)
